cf15a0b0f25702716ba68adaedb14f17482c19e8
korone <120593246+chikichikibangbang@users.noreply.github.com>
2025-04-05 15:17:11 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": 'Modder Batch Tool',
     "author": '诸葛不太亮',
     "blender": (2, 93, 0),
-    "version": (1, 3),
+    "version": (1, 3, 1),
     "description": 'Utility tools to do a lot of repetitive operations automatically.',
     "warning": '',
     "wiki_url": 'https://github.com/chikichikibangbang/Modder_Batch_Tool',
